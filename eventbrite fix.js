26416508a24d449c9b95f3b9dc91908b3c7a0290
Xiagu <xiagu.zepheron@gmail.com>
2014-02-04 23:22:27 -0500

    Shrink floating <td>
    The floating <td> element was getting padded and blocking click events
    to following rows. I excluded it from the selector.

--- eventbrite fix.js
+++ eventbrite fix.js
@@ -101,11 +101,11 @@ jQuery("#checkin_table").click(function(event) {
             floatingTD.html("<table class='report'>" + table.html() + "</table>");
 
             var attendeeTable = floatingTD.find("table");
-            jrow.children("td").css("padding-bottom", attendeeTable.height() + 15);
+            jrow.children("td").not(".loading").css("padding-bottom", attendeeTable.height() + 15);
             // I'm not sure this actually ever does anything
             attendeeTable.resize(function() {
                 console.log("Resize handler called");
-                jrow.children("td").css("padding-bottom", attendeeTable.height() + 15);
+                jrow.children("td").not(".loading").css("padding-bottom", attendeeTable.height() + 15);
             });
         });
     }
